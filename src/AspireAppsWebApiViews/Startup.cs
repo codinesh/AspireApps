d6445d48d530d7ff452a73c4bdd89e1cb12f8ed3
Dinesh Kumar <codinesh@live.com>
2015-08-16 23:54:19 +0530

    minor fix.

--- src/AspireAppsWebApiViews/Startup.cs
+++ src/AspireAppsWebApiViews/Startup.cs
@@ -68,7 +68,7 @@ namespace AspireAppsWebApiViews
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=AddSurvey}/{id?}");
+                    template: "{controller=Survey}/{action=Add}/{id?}");
 
                 // Uncomment the following line to add a route for porting Web API 2 controllers.
                 // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
